5cedc427223d32ef2ddddc1ba9b7b40c60c9369d
Giulia Tommasi <giulia.tommasi.4@studenti.unipd.it>
2023-05-20 22:53:31 +0200

    Added analysis alert view

---
+++ lib/progress_trace/views/analysis_alert_view.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import '../../Constants/pregnancy_health_app_theme.dart';
+
+class AnalysisAlertView extends StatelessWidget {
+  final AnimationController? animationController;
+  final Animation<double>? animation;
+
+  const AnalysisAlertView({Key? key, this.animationController, this.animation})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animationController!,
+      builder: (BuildContext context, Widget? child) {
+        return FadeTransition(
+          opacity: animation!,
+          child: Transform(
+            transform: Matrix4.translationValues(
+                0.0, 30 * (1.0 - animation!.value), 0.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 24, right: 24, top: 0, bottom: 0),
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16, bottom: 16),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: PHAppTheme.white,
+                            borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(8.0),
+                                bottomLeft: Radius.circular(8.0),
+                                bottomRight: Radius.circular(8.0),
+                                topRight: Radius.circular(8.0)),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                  color: PHAppTheme.grey.withOpacity(0.4),
+                                  offset: const Offset(1.1, 1.1),
+                                  blurRadius: 10.0),
+                            ],
+                          ),
+                          child: Stack(
+                            alignment: Alignment.topLeft,
+                            children: <Widget>[
+                              ClipRRect(
+                                borderRadius: const BorderRadius.all(
+                                    Radius.circular(8.0)),
+                                child: SizedBox(
+                                  height: 74,
+                                  child: AspectRatio(
+                                    aspectRatio: 1.714,
+                                    child: Image.asset(
+                                        "assets/images/progress_trace/back.png"),
+                                  ),
+                                ),
+                              ),
+                              const Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                            left: 100,
+                                            right: 16,
+                                            top: 30,
+                                            bottom: 25),
+                                        child: Text(
+                                          "In the last 24 hours, you walked 7000 steps and exceeded of 300 Kcal. Keep an eye.",
+                                          textAlign: TextAlign.left,
+                                          style: TextStyle(
+                                            fontFamily: PHAppTheme.fontName,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 14,
+                                            letterSpacing: 0.0,
+                                            color: PHAppTheme.nearlyDarkBlue,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: -16,
+                        left: 0,
+                        child: SizedBox(
+                          width: 110,
+                          height: 110,
+                          child: Image.asset(
+                              "assets/images/progress_trace/runner.png"),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
