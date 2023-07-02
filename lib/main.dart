f51a3a2567a30cdc9eb0751d6bd4e89dc525e00d
Giulia Tommasi <giulia.tommasi.4@studenti.unipd.it>
2023-07-02 11:46:49 +0200

    Added windowd size on Windows platform

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@ import 'package:exam/database/database.dart';
 import 'package:exam/repositories/calories_repository.dart';
 import 'package:exam/repositories/sleep_repository.dart';
 import 'package:exam/repositories/steps_repository.dart';
+import 'package:exam/repositories/user_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'app.dart';
+import 'package:window_size/window_size.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:io' show Platform;
 
 void main() async {
   await dotenv.load(fileName: ".env");
@@ -23,6 +27,9 @@ void main() async {
   final stepsRepository = StepsRepository(database: database);
   final caloriesRepository = CaloriesRepository(database: database);
   final sleepRepository = SleepRepository(database: database);
+  final userRepository = UserRepository(database: database);
+
+  setupWindow();
 
   runApp(MultiProvider(
     providers: [
@@ -32,9 +39,31 @@ void main() async {
           create: (context) => caloriesRepository),
       ChangeNotifierProvider<SleepRepository>(
           create: (context) => sleepRepository),
+      ChangeNotifierProvider<UserRepository>(
+        create: (context) => userRepository,
+      )
     ],
     child: const App(),
   ));
+}
+
+const double windowWidth = 428;
+const double windowHeight = 926;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Provider Demo');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(Rect.fromCenter(
+        center: screen!.frame.center,
+        width: windowWidth,
+        height: windowHeight,
+      ));
+    });
+  }
 }
 
 class HexColor extends Color {
