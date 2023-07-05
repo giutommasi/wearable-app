f61e7e8344d747ef0185becbce181dd0f004ca24
Giulia Tommasi <giulia.tommasi.4@studenti.unipd.it>
2023-07-05 20:39:35 +0200

    Merged feature-nicolo and feature-giutommasi

---
+++ lib/Pages/food_plan/widget/tips.dart
@@ -0,0 +1,23 @@
+class Tips{
+  final String webPath, imagePath;
+  final int numbTips;
+
+  Tips({
+    required this.numbTips,
+    required this.webPath,
+    required this.imagePath,
+  });
+}
+
+final tips1 = [
+  Tips(
+    numbTips: 1,
+    imagePath: 'https://www.salute.gov.it/imgs/C_17_pagineAree_5692_0_immagine.jpg',
+    webPath: 'https://www.salute.gov.it/portale/saluteBambinoAdolescente/dettaglioContenutiSaluteBambinoAdolescente.jsp?lingua=italiano&id=5692&area=saluteBambino&menu=azioni',
+  ),
+  Tips(
+    numbTips: 2,
+    imagePath: 'https://www.salute.gov.it/imgs/C_17_pagineAree_5702_0_immagine.jpg',
+    webPath: 'https://www.salute.gov.it/portale/saluteBambinoAdolescente/dettaglioContenutiSaluteBambinoAdolescente.jsp?lingua=italiano&id=5702&area=saluteBambino&menu=azioni',
+  ),
+  ];
