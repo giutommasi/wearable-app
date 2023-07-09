53aa470463fd034f11b010d2b31db511f9667e86
Varottonico <119740252+Varottonico@users.noreply.github.com>
2023-07-09 11:47:46 +0200

    Add files via upload

---
+++ weekTips.dart
@@ -0,0 +1,48 @@
+class PregnancyWeek {
+  final int week;
+  final String link;
+
+  PregnancyWeek({required this.week, required this.link});
+}
+
+List<PregnancyWeek> pregnancyWeeks = [
+  PregnancyWeek(week: 1, link: 'https://www.nhs.uk/pregnancy/week-by-week/1-to-12/1-2-3-weeks/'),
+  PregnancyWeek(week: 2, link: 'https://www.nhs.uk/pregnancy/week-by-week/1-to-12/4-weeks/'),
+  PregnancyWeek(week: 3, link: 'https://www.nhs.uk/pregnancy/week-by-week/1-to-12/5-weeks/'),
+  PregnancyWeek(week: 4, link: 'https://www.nhs.uk/pregnancy/week-by-week/1-to-12/6-weeks/'),
+  PregnancyWeek(week: 5, link: 'https://www.nhs.uk/pregnancy/week-by-week/1-to-12/7-weeks/'),
+  PregnancyWeek(week: 6, link: 'https://www.nhs.uk/pregnancy/week-by-week/1-to-12/8-weeks/'),
+  PregnancyWeek(week: 7, link: 'https://www.nhs.uk/pregnancy/week-by-week/1-to-12/9-weeks/'),
+  PregnancyWeek(week: 8, link: 'https://www.nhs.uk/pregnancy/week-by-week/1-to-12/10-weeks/'),
+  PregnancyWeek(week: 9, link: 'https://www.nhs.uk/pregnancy/week-by-week/1-to-12/11-weeks/'),
+  PregnancyWeek(week: 10, link: 'https://www.nhs.uk/pregnancy/week-by-week/1-to-12/12-weeks/'),
+  PregnancyWeek(week: 11, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/13-weeks/'),
+  PregnancyWeek(week: 12, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/14-weeks/'),
+  PregnancyWeek(week: 13, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-15/'),
+  PregnancyWeek(week: 14, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-16/'),
+  PregnancyWeek(week: 15, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-17/'),
+  PregnancyWeek(week: 16, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-18/'),
+  PregnancyWeek(week: 17, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-19/'),
+  PregnancyWeek(week: 18, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-20/'),
+  PregnancyWeek(week: 19, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-21/'),
+  PregnancyWeek(week: 20, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-22/'),
+  PregnancyWeek(week: 21, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-23/'),
+  PregnancyWeek(week: 22, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-24/'),
+  PregnancyWeek(week: 23, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-25/'),
+  PregnancyWeek(week: 24, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-26/'),
+  PregnancyWeek(week: 25, link: 'https://www.nhs.uk/pregnancy/week-by-week/13-to-27/week-27/'),
+  PregnancyWeek(week: 26, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-28/'),
+  PregnancyWeek(week: 27, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-29/'),
+  PregnancyWeek(week: 28, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-30/'),
+  PregnancyWeek(week: 29, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-31/'),
+  PregnancyWeek(week: 30, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-32/'),
+  PregnancyWeek(week: 31, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-33/'),
+  PregnancyWeek(week: 32, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-34/'),
+  PregnancyWeek(week: 33, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-35/'),
+  PregnancyWeek(week: 34, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-36/'),
+  PregnancyWeek(week: 35, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-37/'),
+  PregnancyWeek(week: 36, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-38/'),
+  PregnancyWeek(week: 37, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-39/'),
+  PregnancyWeek(week: 38, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-40/'),
+  PregnancyWeek(week: 39, link: 'https://www.nhs.uk/pregnancy/week-by-week/28-to-40-plus/week-41/'),
+];
